48b21187fd20b7077064936fc38931733c7c39ba
Hyeon_Wooooo <84663334+po2955@users.noreply.github.com>
2024-03-07 21:41:50 +0900

    Title: 코드트리 빵, Time: 251ms, Memory: 26MB, Status: Passed - CodeTree

--- 240307/코드트리 빵/codetree-mon-bread.py
+++ 240307/코드트리 빵/codetree-mon-bread.py
@@ -7,19 +7,21 @@ board_people = [[[] for _ in range(n)] for _ in range(n)]
 for i in range(1, m+1):
     x, y = map(int, input().split())
     board[x-1][y-1] = 100 + i
-
-cant_base = []
-cant_store = []
-xxx_yyy = []
-
-def place_people_base():
-    global minute
+cant_pass = []
+def place_people():
     for i in range(n):
         for j in range(n):
-            if board[i][j] == 100 + minute:
+            if board_people[i][j]:
+                return True
+    return False
+
+def base_people():
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == minute + 100:
                 Q = deque()
-                Q.append((i, j))
                 visited = [[0] * n for _ in range(n)]
+                Q.append((i, j))
                 visited[i][j] = 1
                 while Q:
                     temp = Q.popleft()
@@ -28,93 +30,90 @@ def place_people_base():
                         ny = temp[1] + dy[k]
                         if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0 and board[nx][ny] >= 0:
                             visited[nx][ny] = 1
-                            Q.append((nx, ny))
+                            Q.append((nx ,ny))
                             if board[nx][ny] == 1:
+                                # board[nx][ny] = -1
                                 board_people[nx][ny].append(minute)
-                                cant_base.append((nx, ny))
+                                cant_pass.append((nx, ny))
                                 return
 
-def find_convinience_store(x, y, people):
-    # people += 100
-    Q = deque()
-    Q.append((x, y))
-    visited = [[-1] * n for _ in range(n)]
-    visited[x][y] = 0
-    board_trace = [[None] * n for _ in range(n)]
-    while Q:
-        temp = Q.popleft()
-        for i in range(4):
-            nx = temp[0] + dx[i]
-            ny = temp[1] + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] >= 0 and visited[nx][ny] == -1:
-                visited[nx][ny] = visited[temp[0]][temp[1]] + 1
-                board_trace[nx][ny] = (temp[0], temp[1])
-                Q.append((nx, ny))
-                if board[nx][ny] == people + 100:
-                    if visited[nx][ny] == 1:
-                        cant_store.append((nx,ny))
-                        return
-                    xx, yy = board_trace[nx][ny]
-                    while xx != x or yy != y:
-                        xxx, yyy = xx, yy
-                        xx, yy = board_trace[xx][yy]
-                    xxx_yyy.append((xxx, yyy, people))
-                    # board_people[xxx][yyy].append(people)
-                    return
-
-def move_people():
+def clear(move):
     for i in range(n):
         for j in range(n):
             if board_people[i][j]:
-                while board_people[i][j]:
-                    people = board_people[i][j].pop()
-                    find_convinience_store(i, j, people)
-    if xxx_yyy:
-        for x, y, people in xxx_yyy:
+                board_people[i][j].clear()
+
+    if move:
+        for x, y, people in move:
             board_people[x][y].append(people)
 
-def cant_pass():
-    if cant_base:
-        for x, y in cant_base:
-            board[x][y] = -1
-        cant_base.clear()
-    if cant_store:
-        for x, y in cant_store:
-            board[x][y] = -(board[x][y])
-        cant_store.clear()
-
-def is_Finished():
-    cnt = 0
+def move_people():
+    move = []
     for i in range(n):
         for j in range(n):
-            if board[i][j] < -100:
-                cnt += 1
-    if cnt == m:
-        return True
-    else:
-        return False
+            if board[i][j] >= 100:
+                if board[i][j] - 100 >= minute:
+                    continue
+                people = board[i][j] - 100
+                board_trace = [[None] * n for _ in range(n)]
+                Q = deque()
+                visited = [[-1] * n for _ in range(n)]
+                Q.append((i,j))
+                visited[i][j] = 0
+                signal = 0
+                while Q:
+                    temp = Q.popleft()
+                    for k in range(4):
+                        nx = temp[0] + dx[k]
+                        ny = temp[1] + dy[k]
+                        if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == -1:
+                            if board[nx][ny] < 0 and people not in board_people[nx][ny]:
+                                continue
+                            visited[nx][ny] = visited[temp[0]][temp[1]] + 1
+                            Q.append((nx, ny))
+                            board_trace[nx][ny] = (temp[0], temp[1])
+                            
+                            if people in board_people[nx][ny]:
+                                if visited[nx][ny] == 1:
+                                    xx, yy = board_trace[nx][ny]
+                                    cant_pass.append((xx, yy))
+                                    signal = 1
+                                else:
+                                    xx, yy = board_trace[nx][ny]
+                                    move.append((xx, yy, people))
+                                    signal = 1
+                        if signal == 1:
+                            break
+                    if signal == 1:
+                        break
+    clear(move)
+    
+
+def ban():
+    if cant_pass:
+        for x, y in cant_pass:
+            board[x][y] = -(board[x][y])
+        cant_pass.clear()
+
+def is_Finished():
+    for i in range(n):
+        for j in range(n):
+            if board_people[i][j]:
+                return False
+    return True
 
 minute = 1
-place_people_base()
+base_people()
+ban()
 minute = 2
-round = 1
 while 1:
     move_people()
-    cant_pass()
+    ban()
     if minute <= m:
-        place_people_base()
-    cant_pass()
-    # for x in board:
-    #     print(x, end =' ')
-    #     print()
-    # print('-------------')
-    # for x in board_people:
-    #     print(x, end =' ')
-    #     print()
-    # print()
-
+        base_people()
+    ban()
     if is_Finished() == True:
         break
     minute += 1
-    round += 1
+
 print(minute)
