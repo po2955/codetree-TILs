8ae0dcbbc7a75ab5576cac4337182f8aa691aa0c
Hyeon_Wooooo <84663334+po2955@users.noreply.github.com>
2024-09-10 21:06:32 +0900

    Title: 나무박멸, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/나무박멸/tree-kill-all.py
@@ -0,0 +1,107 @@
+from copy import deepcopy
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+dxc = [-1, -1, 1, 1]
+dyc = [-1, 1, 1, -1]
+
+n, m, k, c = map(int, input().split())
+answer = 0
+board = [list(map(int, input().split())) for _ in range(n)]
+board_killer = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    signal = 0
+    for j in range(n):
+        if board[i][j] == -1:
+            board[i][j] = -11
+            break
+            signal = 1
+    if signal == 1:
+        break
+
+def grow_tree():
+    global board
+    test_board = deepcopy(board)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > 0:
+                for k in range(4):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    if 0 <= nx < n and 0 <= ny < n and board[nx][ny] > 0:
+                        test_board[i][j] += 1
+    board = test_board
+
+def spread_tree():
+    global board
+    test_board = deepcopy(board)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > 0:
+                cnt = 0
+                check_blank = []
+                for q in range(4):
+                    nx = i + dx[q]
+                    ny = j + dy[q]
+                    if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
+                        cnt += 1
+                        check_blank.append((nx, ny))
+                if cnt >= 1:
+                    for x, y in check_blank:
+                        test_board[x][y] += board[i][j] // cnt
+    board = test_board
+
+def choice_killer():
+    global k, answer
+    check = []
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > 0:
+                cnt = board[i][j]
+                for q in range(4):
+                    for p in range(1, k + 1):
+                        nx = i + (dxc[q] * p)
+                        ny = j + (dyc[q] * p)
+                        if 0 <= nx < n and 0 <= ny < n:
+                            if board[nx][ny] == 0 or board[nx][ny] == -11:
+                                break
+                            if board[nx][ny] > 0:
+                                cnt += board[nx][ny]
+                check.append((cnt, i, j))
+    if check:
+        check = sorted(check, key = lambda x : (-x[0], x[1], x[2]))
+        answer += check[0][0]
+        return check[0][1], check[0][2]
+    return -1, -1
+
+def spread_killer(x, y):
+    global k
+    board[x][y] = -(c+1)
+    for q in range(4):
+        for p in range(1, k + 1):
+            nx = x + (dxc[q] * p)
+            ny = y + (dyc[q] * p)
+            if 0 <= nx < n and 0 <= ny < n:
+                if board[nx][ny] == 0:
+                    board[nx][ny] = -(c+1)
+                    break
+                elif board[nx][ny] == -11:
+                    break
+                else:
+                    board[nx][ny] = -(c+1)
+
+
+def count_killer():
+    for i in range(n):
+        for j in range(n):
+            if -11 < board[i][j] < 0:
+                board[i][j] += 1
+
+for turn in range(1, m + 1):
+    grow_tree()
+    spread_tree()
+    kill_x, kill_y = choice_killer()
+    if kill_x != -1:
+        spread_killer(kill_x, kill_y)
+    count_killer()
+print(answer)
